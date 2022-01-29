c66306d5e16695208408e675aeb6533f4f8a6b91
Hammerill <www.hammerill@gmail.com>
2022-01-29 20:04:42 +0200

    Done

--- src/org/hammerill/Main.java
+++ src/org/hammerill/Main.java
@@ -3,6 +3,25 @@ package org.hammerill;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[][] table = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                table[i][j] = (int)(Math.random() * 3);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (table[i][j] == 1) {
+                    System.out.print("X ");
+                } else if (table[i][j] == 2) {
+                    System.out.print("O ");
+                } else {
+                    System.out.print("_ ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
